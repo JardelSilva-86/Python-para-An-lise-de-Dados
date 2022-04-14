5e103132e415211f0873d422b51e0a7578b2e86c
JardelSilva <53876124+JardelSilva-86@users.noreply.github.com>
2022-04-14 17:18:58 -0300

    Create LoopWhile.py

---
+++ LoopWhile.py
@@ -0,0 +1,9 @@
+'''
+Loop While 
+
+O loop While é utilizado para executar um bloco de instruções até que uma determinada condição seja satisfeita. E quando a condição se torna falsa, a linha imediatamente após o programa é executada. O loop While se enquadra na categoria de iteração indefinida. A iteração indefinida significa que o número de vezes que o loop é executado não é específicada explicitamente com antecedência.
+
+Sintaxe:
+While <expressao>
+'''
+
