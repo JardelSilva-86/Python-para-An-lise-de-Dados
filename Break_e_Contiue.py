17ee14bd0f07889510570990a43bf646dfd54dee
JardelSilva <53876124+JardelSilva-86@users.noreply.github.com>
2022-04-18 14:59:33 -0300

    Update Break_e_Contiue.py

--- Break_e_Contiue.py
+++ Break_e_Contiue.py
@@ -4,6 +4,21 @@ Instruções de controle - Break ou Continue
 O uso de loops no Python automatiza e repete as tarefas de maneira efeciente. Mas, às vezes, pode surgir uma condição em que você deseja sair completmentedo loop, pular uma iteração ou ignorar essa condição. Isso pode ser feito por instruções de controle de loop. As instruções de controle de loop alteram a execução de sua sequência normal. Quando a execução sai de um escopo, todos os objetos automáticos que foram criados nesse escopo são destruídos. Python suporta as seguintes instruções de controle. 
 '''
 
-# BREAK
+# BREAK - Interrompe o programa quando ele chegar na condição informada.
 
-Lista =
+Lista = ['Morango', 'Pera', 'Uva', 'Abacaxi']
+
+for fruta in Lista:
+  print ( fruta )
+  if fruta == 'Pera':
+    break
+    
+
+# CONTINUE - Pula a iteração informada na condição.
+
+for Loop in  range ( 0, 11 ):
+  if Loop == 5:
+    continue
+    print( 'Cheguei aqui mais vou ser ignorado!' )
+
+  print( Loop )
