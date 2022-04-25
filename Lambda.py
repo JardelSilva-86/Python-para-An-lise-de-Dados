64912e570beb9c0dd1bd38370137379c2d07cc64
JardelSilva <53876124+JardelSilva-86@users.noreply.github.com>
2022-04-25 17:57:56 -0300

    Create Lambda.py

---
+++ Lambda.py
@@ -0,0 +1,14 @@
+# Uma função lambda é uma pequena função anônima.
+# Uma função lambda pode receber qualquer número de argumentos, mas pode ter apenas uma expressão.
+
+# sintaxe: <variavel> = lambda <qqr valor> : <expressao>
+
+# Soma
+funcao_soma = lambda valor: valor + 10
+
+funcao_soma(25)
+
+# Multiplica
+funcao_multiplica = lambda valor_1, valor_2: valor_1 * valor_2
+
+funcao_multiplica(98, 2)
