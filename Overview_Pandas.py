27376076b207d3d3a85f67d852fc2bc97e83dd4b
JardelSilva <53876124+JardelSilva-86@users.noreply.github.com>
2022-05-03 15:53:22 -0300

    Update Overview_Pandas.py

--- Overview_Pandas.py
+++ Overview_Pandas.py
@@ -5,3 +5,35 @@
 import pandas as pd
 import numpy as np
 
+# Utilizando o Google Colab
+df = pd.read_csv('/content/StudentsPerformance.csv')
+
+# Tipo da variável
+type( df )
+
+# As 5 primeiras linhas
+df.head()
+
+# As 5 últimas linhas
+df.tail()
+
+# Quantidade de linhas e colunas
+df.shape
+
+# Retornar um array com o nome das colunas
+df.columns
+
+# Verificar linhas duplicadas e somá-las
+df.duplicated().sum()
+
+# Retornar informações genéricas sobre o df
+df.info()
+
+# Retornar a quantidade de valores missing/Not a Number(NaN) e somá-los
+df.isna().sum()
+
+# Retornar um sumário estatístico
+df.describe()
+
+# Retornar um sumário estatístico para variáveis categóricas
+df.describe(include = 'all')
